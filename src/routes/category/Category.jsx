9c179dec6cd8eeee176287a6692827d3a0c13c84
@azick99 <yunusaliyevazizbek80@gmail.com>
2023-03-28 13:35:09 +0200

    updating redirect

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import ProductCard from '../../components/product-card/ProductCard'
 import { CategoriesContext } from '../../context/categories.context'
 import './category.syle.scss'
@@ -8,14 +8,15 @@ const Category = () => {
   const { title } = useParams()
   const { categories } = useContext(CategoriesContext)
   const [products, setProducts] = useState(categories[title])
-
+  const navigate = useNavigate()
+  const navigateCollectionsHandler = () => navigate('/collections')
   useEffect(() => {
     setProducts(categories[title])
   }, [categories, title])
 
   return (
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
+      <h1 onClick={navigateCollectionsHandler} className="category-title">{title.toUpperCase()}</h1>
       <div className="category-container">
         {products &&
           products.map((product) => (
