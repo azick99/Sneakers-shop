cb3f0a698fb8f65b97f4765ed14ce63d0d3f8f14
@azick99 <yunusaliyevazizbek80@gmail.com>
2023-04-22 12:59:56 +0200

    addition

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import { userReducer } from './user/user.reducer'
-import {toggleReducer} from './toggles/toggle.reducer'
 import { categoriesReducer } from './categories/category.reducer'
 import { cartReducer } from './cart/cart.reducer'
 
 export const rootReducer = combineReducers({
   user: userReducer,
-  toggle: toggleReducer,
   categories: categoriesReducer,
   cart: cartReducer,
 })
